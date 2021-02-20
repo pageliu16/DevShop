938cc92c9924292a3cc2ed0cd3ea8c4865c9bf68
pageliu16 <pageliu16@qq.com>
2021-02-20 14:20:09 +0800

    2021-02-20

--- Hello/Hello/Form1.Designer.cs
+++ Hello/Hello/Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace Hello
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(300, 189);
+            this.button1.Location = new System.Drawing.Point(164, 180);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 71);
             this.button1.TabIndex = 0;
@@ -46,7 +46,7 @@ namespace Hello
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(515, 450);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
